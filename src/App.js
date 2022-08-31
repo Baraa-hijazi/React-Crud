47a1d6de8f5413f51750b5b36e15f7fab486a067
ACACUSGROUP\b.hejazi <bqumsan99@gmail.com>
2022-08-31 16:38:15 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from './Navbar'
+import Users from './Users'
+import UserCreate from './UserCreate'
+// import axios from 'axios';
+//
+// axios.defaults.baseURL = 'http://localhost:7000/'
+// axios.defaults.headers.common = {'Authorization': `${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsIlVzZXJuYW1lIjoiYWRtaW4iLCJqdGkiOiI0NWQwZTM5Yy0xNjcwLTRjM2YtOGM1Ny03OGZiOGE4ZTYwM2EiLCJpYXQiOjYzNzk3NTUwNjA4OTE4MDI0MSwibmJmIjoxNjYxOTQzMDA4LCJleHAiOjE2NjIwMjk0MDgsImlzcyI6IkFkbWluIiwiYXVkIjoiQWRtaW4ifQ.rIZ8EfzDR9CUys5aKAp9zWPKxGit_yzB-z5_RiLtqAc}`}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Navbar/>
+                <Users/>
+
+                <Routes>
+                    <Route exact path='/' component={Users}/>
+                    <Route exact path='/create' component={UserCreate}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
-
-export default App;
